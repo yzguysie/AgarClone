a6ecdafe5d235871a4083182bb994ab9b9c3458a
yzguysie <52264307+yzguysie@users.noreply.github.com>
2024-04-27 15:26:22 -0500

    Increase sent data size

--- network.py
+++ network.py
@@ -24,14 +24,14 @@ class Network:
     def connect(self):
         try:
             self.client.connect(self.addr)
-            return pickle.loads(self.client.recv(4096*4096*2))
+            return pickle.loads(self.client.recv(4096*4096*16))
         except:
             pass
 
     def send(self, data):
         try:
             self.client.send(pickle.dumps(data))
-            return pickle.loads(self.client.recv(4096*4096*2))
+            return pickle.loads(self.client.recv(4096*4096*16))
         
         except socket.error as e:
             print(e)
