10de438407d4ffb5b7702302fdaca91e8e5922be
yzguysie <52264307+yzguysie@users.noreply.github.com>
2024-05-04 20:11:10 -0500

    Update serverchanges.py

--- common/serverchanges.py
+++ common/serverchanges.py
@@ -1,6 +1,8 @@
 class ServerChanges:
+    batch_id = 0
     objects_added = set()
     objects_deleted = set()
     players = []
     cells = []
     ejected = []
+    next_batch: "ServerChanges" = None
